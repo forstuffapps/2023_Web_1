f6332dff811faf737652af65cf380f03bca9c01d
forstuffapps <forstuffapps@gmail.com>
2023-08-25 19:19:46 -0400

    completed the entire javascript

--- Section_18 - Drum Kit/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Section_18 - Drum Kit/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -2,14 +2,73 @@
 
 
 
+function trigger_audio(q){
+    switch (q) {
+        case "w":
+            var a1 = new Audio("./sounds/crash.mp3")
+            a1.play();
+            break;
+        
+        case "a":
+            var a2 = new Audio("./sounds/kick-bass.mp3")
+            a2.play();
+            break;
+
+        case "s":
+            var a3 = new Audio("./sounds/snare.mp3")
+            a3.play();
+            break;
+    
+        case "d":
+            var a4 = new Audio("./sounds/tom-1.mp3")
+            a4.play();
+            break;
+            
+        case "j":
+            var a5 = new Audio("./sounds/tom-2.mp3")
+            a5.play();
+            break;
+        
+        case "k":
+            var a6 = new Audio("./sounds/tom-3.mp3")
+            a6.play();
+            break;
+            
+        case "l":
+            var a7 = new Audio("./sounds/tom-4.mp3")
+            a7.play();
+            break;
+            
+        default:
+            break;
+    }
+}
+
+
 
 for(i=0;i<7;i++){
     document.getElementsByClassName("drum")[i].addEventListener("click", function(){
-        var audio = new Audio('./sounds/crash.mp3');
-        audio.play();
+        trigger_audio(this.innerHTML);
+        button_Animation(this.innerHTML);
     });
 }
 
+
+
+document.addEventListener("keydown", function(event){
+        trigger_audio(event.key);
+        button_Animation(event.key);
+    });
+
+
+
+
+
+    function button_Animation(k){
+        var button = document.querySelector("."+k);
+        button.classList.add("pressed");
+        setTimeout(function(){button.classList.remove("pressed")}, 100);
+    }
 
 
 
