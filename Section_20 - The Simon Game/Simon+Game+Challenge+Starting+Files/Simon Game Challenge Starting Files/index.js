75f07aea00e1c41f2c979145c1d831a3f8d36ba7
forstuffapps <forstuffapps@gmail.com>
2023-08-26 01:06:22 -0400

    started a boss level game 2

---
+++ Section_20 - The Simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+$(document).keypress(function(event){
+    var game=true;
+    const A = [];
+    var n=0;
+    var k = 0;
+
+    while(game==true){
+        n+=1;
+        k=Math.random()*4+1;
+        A.push(k);
+        
+        for(i=0;i<n;i++){
+            var t = $(document).click()
+        }
+
+
+    }
+});
+
+
+
+
