8a3e4a9af3c31f7799b0949264ef61316cc9db37
forstuffapps <forstuffapps@gmail.com>
2023-09-16 18:59:51 -0400

    did the assignment secret html file

--- Section_24 - Express JS/3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ Section_24 - Express JS/3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -1,3 +1,56 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express();
+const port = 3000;
+var matched = false;
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+function secrets(req, res, next) {
+    
+    if (req.body["password"]=="ILoveProgramming")
+    {
+        matched = true;
+    }
+    next();
+}
+
+
+app.use(secrets)
+
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+
+app.post("/check", (req, res) =>{
+    if(matched)
+    {
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else
+    {
+        res.sendFile(__dirname + "/public/index.html")
+    }
+});
+
+
+app.listen(port, () =>{
+    console.log(`Listening on port number ${port}`);
+})
+
+
+
