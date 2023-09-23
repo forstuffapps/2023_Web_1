cc0c0fce5c931e3e640c5ef4880a9553b747a74c
forstuffapps <forstuffapps@gmail.com>
2023-09-23 02:56:34 -0400

    re did the secret project

--- Section_24 - Express JS/3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ Section_24 - Express JS/3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -11,53 +11,46 @@ import bodyParser from "body-parser";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+
 const app = express();
 const port = 3000;
-var matched = false;
+var authorised = false;
 
 
+app.use(bodyParser.urlencoded({ extended : true }));
 
-function secrets(req, res, next) {
-    
+function secrets(req, res, next)
+{
     if (req.body["password"]=="ILoveProgramming")
     {
-        //matched = true;
-        res.sendFile(__dirname + "/public/secret.html");
-    }
-    else
-    {
-        res.sendFile(__dirname + "/public/index.html")
+        authorised = true;
     }
     next();
 }
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(secrets);
 
-//app.use(secrets)
-
-
-app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req,res) =>
+{
+    res.sendFile(__dirname + "/public/index.html")
 })
 
 
-app.post("/check", secrets);
-/*
-(req, res) =>{
-    if(matched)
+app.post("/check", (req,res) =>
+{
+    if (authorised)
     {
-        res.sendFile(__dirname + "/public/secret.html");
+        res.sendFile(__dirname + "/public/secret.html")
     }
     else
     {
         res.sendFile(__dirname + "/public/index.html")
     }
-});
-*/
+})
 
-
-app.listen(port, () =>{
-    console.log(`Listening on port number ${port}`);
+app.listen(port, () =>
+{
+    console.log(`Litening on port ${port}`);
 })
 
 
