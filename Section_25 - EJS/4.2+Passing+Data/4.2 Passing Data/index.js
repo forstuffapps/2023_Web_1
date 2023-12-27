12cb348bff6cb849ee105201b22820d6a2f06671
forstuffapps <forstuffapps@gmail.com>
2023-12-26 20:25:46 -0500

    qwe

--- Section_25 - EJS/4.2+Passing+Data/4.2 Passing Data/index.js
+++ Section_25 - EJS/4.2+Passing+Data/4.2 Passing Data/index.js
@@ -5,10 +5,17 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+var cnt = 0;
+app.get("/", (req, res) => {
+  res.render("index.ejs", {count : cnt})
+});
 
-app.get("/", (req, res) => {});
-
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  cnt=0
+  cnt+= req.body["fName"].length
+  cnt += req.body["lName"].length
+  res.render("index.ejs", {count : cnt})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
