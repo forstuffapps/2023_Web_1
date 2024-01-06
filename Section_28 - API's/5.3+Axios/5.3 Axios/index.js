0c75240639ae7d32b10807272bf2ab454b82daf2
forstuffapps <forstuffapps@gmail.com>
2024-01-05 21:44:58 -0500

    completed the 5.3 API's Axios task

--- Section_28 - API's/5.3+Axios/5.3 Axios/index.js
+++ Section_28 - API's/5.3+Axios/5.3 Axios/index.js
@@ -35,6 +35,21 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+
+  var url = "https://bored-api.appbrewery.com/filter?type=" + req.body.type + "&participants=" + req.body.participants
+  console.log(url)
+  try {
+    const response = await axios.get(url);
+    const result = response.data;
+    res.render("index.ejs", { data: result[0] });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
+
+
 });
 
 app.listen(port, () => {
